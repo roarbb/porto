df8338887b32e2499486b1e6bf2a983cde456982
Matej Sajgal <matej@sajgal.com>
2017-07-13 21:41:53 +1000

    updated proc file

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -15,13 +15,12 @@ var _Body2 = _interopRequireDefault(_Body);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/msajgal/Developer/porto/pages/index.js?entry';
-// import Link from 'next/link'
 
 exports.default = function () {
     return _react2.default.createElement(_Body2.default, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 6
+            lineNumber: 5
         }
     }, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolorem tempore ad aut soluta laborum pariatur consequuntur ipsam. Dolore esse nulla harum necessitatibus minus repellat, culpa nam quia provident corrupti!');
 };
