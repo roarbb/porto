cde5c37614830a6f1805a23ead5823b689829be4
Matej Sajgal <matej@sajgal.com>
2017-07-13 21:26:12 +1000

    init

---
+++ .next/dist/components/Body.js
@@ -0,0 +1,38 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _head = require('next/dist/lib/head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/msajgal/Developer/porto/components/Body.js';
+// import React from 'react'
+
+exports.default = function (_ref) {
+	var children = _ref.children;
+	return _react2.default.createElement('div', {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 5
+		}
+	}, _react2.default.createElement(_head2.default, {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 6
+		}
+	}, _react2.default.createElement('style', {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 8
+		}
+	}, '\nhtml, body, div, span, applet, object, iframe,\nh1, h2, h3, h4, h5, h6, p, blockquote, pre,\na, abbr, acronym, address, big, cite, code,\ndel, dfn, em, img, ins, kbd, q, s, samp,\nsmall, strike, strong, sub, sup, tt, var,\nb, u, i, center,\ndl, dt, dd, ol, ul, li,\nfieldset, form, label, legend,\ntable, caption, tbody, tfoot, thead, tr, th, td,\narticle, aside, canvas, details, embed, \nfigure, figcaption, footer, header, hgroup, \nmenu, nav, output, ruby, section, summary,\ntime, mark, audio, video {\n\tmargin: 0;\n\tpadding: 0;\n\tborder: 0;\n\tfont-size: 100%;\n\tfont: inherit;\n\tvertical-align: baseline;\n}\n/* HTML5 display-role reset for older browsers */\narticle, aside, details, figcaption, figure, \nfooter, header, hgroup, menu, nav, section {\n\tdisplay: block;\n}\nbody {\n\tline-height: 1;\n}\nol, ul {\n\tlist-style: none;\n}\nblockquote, q {\n\tquotes: none;\n}\nblockquote:before, blockquote:after,\nq:before, q:after {\n\tcontent: \'\';\n\tcontent: none;\n}\ntable {\n\tborder-collapse: collapse;\n\tborder-spacing: 0;\n}\n            ')), children);
+};
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Body from '../components/Body'
+
+export default () => (
+    <Body>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolorem tempore ad aut soluta laborum pariatur consequuntur ipsam. Dolore esse nulla harum necessitatibus minus repellat, culpa nam quia provident corrupti!
+    </Body>
+)
